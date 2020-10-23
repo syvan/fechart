fb1f463255fd8c82a0ea43b40a6c96b20dad6b9e
xiexipeng <xxp@szltech.com>
2020-10-23 13:32:11 +0800

    变更项目结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
 
+import vueHelloWorld from './components/HelloWorld/HelloWorld.js'
+
+Vue.use(vueHelloWorld)
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
