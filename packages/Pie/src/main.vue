ee5e7e1d05ea725c7496dea2400cf99b37253058
xiexipeng <xxp@szltech.com>
2020-11-13 18:24:22 +0800

    完善方法

--- packages/Pie/src/main.vue
+++ packages/Pie/src/main.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="_fe-wrap-pie">
-    <div class="_fe-chart" :id="chartId"></div>
+  <div class="_fe-wrap-pie _fe-wrap">
+    <slot name="tag"></slot>
+    <div class="_fe-chart-pie _fe-chart" :id="chartId"></div>
   </div>
 </template>
 
@@ -10,22 +11,91 @@ export default {
   name: "fe-pie",
   mixins: [chartMix],
   props: {
-    // 图表id
-    chartId: {
-      type: String,
-      default: "chart"
+    // 图表配置
+    options: {
+      type: Object,
+      default: () => ({})
     },
+    dataList: {
+      type: Array,
+      default: () => [
+        {value: 335, name: '直接访问', color: 'red'},
+        {value: 310, name: '邮件营销', color: 'green'},
+        {value: 234, name: '联盟广告', color: 'blue'}
+      ]
+    }
   },
   data() {
     return {
       chart: null,
-      chartOption: {}
+      defaultOption: {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            data: [],
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '20',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+          }
+        ]
+      }
     };
   },
+  computed: {
+    pieDataList() {
+      let legendData = [],  //图例数据
+          seriesData = [],  //图表数据
+          colorData = []   //图例及图表颜色
+      this.dataList.forEach(di => {
+        legendData.push(di.name)
+        seriesData.push({
+          value: di.value,
+          name: di.name
+        })
+        di.color ? colorData.push(di.color) : ''
+      })
+      return {
+        legendData,
+        seriesData,
+        colorData
+      }
+    }
+  },
+  watch: {
+    pieDataList (val) {
+      if (!val) return
+      this.updatePieData(val)
+    }
+  },
   methods: {
-    
+    updatePieData (val) {
+      this.chart.setOption({
+        series: [{
+          data: val.seriesData
+        }],
+        color: val.colorData
+      })
+    }
   },
   mounted() {
+    this.updatePieData(this.pieDataList)
   },
   destroy() {
   }
@@ -38,7 +108,7 @@ export default {
   width: 100%;
   height: 100%;
 }
-._fe-chart {
+._fe-chart-pie {
   width: 100%;
   height: 100%;
   display: block;
--- packages/mixins/chart.js
+++ packages/mixins/chart.js
@@ -1,19 +1,33 @@
-import $ from "jquery"
 import echarts from "echarts"
+import $ from "jquery"
 export default {
+  props: {
+    // 图表id
+    chartId: {
+      type: String,
+      default: "chart"
+    },
+    // 图表配置
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  },
   data() {
     return {
       chart: null,
     }
   },
   created() {
+    let that = this
     $(window).resize(function(){
-      this.resizeCanvas()
+      that.resizeCanvas()
     })
   },
   mounted() {
     /** 调用图表初始化函数 */
     this.initChart()
+    this.updateChart(this.chartOption)
     /** 初始化图表尺寸 */
     this.resizeCanvas()
   },
@@ -24,56 +38,32 @@ export default {
       this.chart = null;
     }
   },
+  computed: {
+    chartOption() {
+      Object.keys(this.options).forEach(opt => {
+        this.defaultOption[opt] = this.options[opt]
+      })
+      return this.defaultOption
+    }
+  },
+  watch: {
+    chartOption(val) {
+      this.updateChart(val)
+    }
+  },
   methods: {
     /** 初始化图表 */
     initChart() {
       //eslint-disable-next-line
       this.chart = echarts.init(document.getElementById("chart"));
-      this.chartOption = {
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 10,
-          data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-        },
-        series: [
-          {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['50%', '70%'],
-            avoidLabelOverlap: false,
-            label: {
-              show: false,
-              position: 'center'
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: '30',
-                fontWeight: 'bold'
-              }
-            },
-            labelLine: {
-              show: false
-            },
-            data: [
-              {value: 335, name: '直接访问'},
-              {value: 310, name: '邮件营销'},
-              {value: 234, name: '联盟广告'},
-              {value: 135, name: '视频广告'},
-              {value: 1548, name: '搜索引擎'}
-            ]
-          }
-        ]
-      };
       this.chart.setOption(this.chartOption);
     },
+    updateChart() {
+      this.chart.setOption(this.chartOption)
+    },
     resizeCanvas() {
-      $("._fe-chart").css("width", $("._fe-wrap-pie").width())
-      $("._fe-chart").css("height", $("._fe-wrap-pie").height())
+      $("._fe-chart").css("width", $("._fe-wrap").width())
+      $("._fe-chart").css("height", $("._fe-wrap").height())
     },
   }
 }
